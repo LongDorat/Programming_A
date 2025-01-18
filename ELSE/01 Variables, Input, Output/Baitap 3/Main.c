df7d68450bc94cfdddad803c51dc8f3d543b99e9
LongDorat <longdorat@outlook.com>
2025-01-18 16:13:59 +0700

    ADD: Variables, Input, Output (BT3)

---
+++ ELSE/01 Variables, Input, Output/Baitap 3/Main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+struct Point
+{
+    double x, y;
+};
+
+void readPoint(struct Point *p)
+{
+    scanf("%lf%lf", &(p->x), &(p->y));
+}
+
+void printPoint(struct Point a)
+{
+    printf("(%.4lf, %.4lf)\n", a.x, a.y);
+}
+
+int main()
+{
+    struct Point a, b;
+    readPoint(&a);
+    readPoint(&b);
+    printPoint(a);
+    printPoint(b);
+
+    float distance = sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+    printf("%.4lf\n", distance);
+
+    return 0;
+}
