c3923c0869a955692bc3f28e81e22f3fb9e164ca
Long Dorat <longdorat@outlook.com>
2025-02-05 07:40:28 +0700

    FIX: How function work It supposed to be print out the array in reverse order.

--- ELSE/04 Array/Baitap 1/Cau 1.c
+++ ELSE/04 Array/Baitap 1/Cau 1.c
@@ -1,18 +1,19 @@
 #include "Include.h"
 
-void reversePrint(int n) {
-    if (n > 0) {
-        reversePrint(n - 1);
-        printf("%d ", n);
-    }
-}
-
 int main()
 {
     int n;
     scanf("%d", &n);
+    int a[1000];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
 
-    reversePrint(n);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", a[i]);
+    }
 
     return 0;
 }
